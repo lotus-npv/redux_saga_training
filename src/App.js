8edef1ef74f35175f80f1e90c4dbed276d2af38c
lotus-npv <lotus.npv@gmail.com>
2024-03-17 08:21:31 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,54 +1,101 @@
-import React from 'react';
+// import React from 'react';
+// import { connect } from 'react-redux';
+
+// import {getListPost as getListPostAction} from './redux/action';
+
+// class App extends React.Component {
+//   render() {
+//     const {posts, load} = this.props.posts;
+
+//     if(load) {
+//       return (
+//         <h1>Data is loading from API...</h1>
+//       )
+//     }
+//     return (
+//       <>
+//         <h1>List Post</h1>
+//         <table>
+//           <tbody>
+//             <tr>
+//               <th>Id</th>
+//               <th>Title</th>
+//             </tr>
+//             {
+//               posts.map((post) => (
+//                 <tr>
+//                   <th>{post.id}</th>
+//                   <th>{post.title}</th>
+//                 </tr>
+//               ))
+//             }
+//           </tbody>
+//         </table>
+//       </>
+//     );
+//   }
+
+//   componentDidMount() {
+//     this.props.getListPost();
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     posts: state.posts,
+//   }
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     getListPost: (params) => dispatch(getListPostAction(params)),
+//   }
+// }
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+
+
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
-import {getListPost as getListPostAction} from './redux/action';
+import { getListPost as getListPostAction } from './redux/action';
 
-class App extends React.Component {
-  render() {
-    const {posts, load} = this.props.posts;
+const App = ({ posts: { posts, load }, getListPost }) => {
+  useEffect(() => {
+    getListPost();
+  }, [getListPost]);
 
-    if(load) {
-      return (
-        <h1>Data is loading from API...</h1>
-      )
-    }
-    return (
-      <>
-        <h1>List Post</h1>
-        <table>
-          <tbody>
-            <tr>
-              <th>Id</th>
-              <th>Title</th>
+  if (load) {
+    return <h1>Data is loading from API...</h1>;
+  }
+
+  return (
+    <>
+      <h1>List Post</h1>
+      <table>
+        <tbody>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+          </tr>
+          {posts.map((post) => (
+            <tr key={post.id}>
+              <td>{post.id}</td>
+              <td>{post.title}</td>
             </tr>
-            {
-              posts.map((post) => (
-                <tr>
-                  <th>{post.id}</th>
-                  <th>{post.title}</th>
-                </tr>
-              ))
-            }
-          </tbody>
-        </table>
-      </>
-    );
-  }
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
-  componentDidMount() {
-    this.props.getListPost();
-  }
-}
+const mapStateToProps = (state) => ({
+  posts: state.posts,
+});
 
-const mapStateToProps = (state) => {
-  return {
-    posts: state.posts,
-  }
-}
+const mapDispatchToProps = (dispatch) => ({
+  getListPost: (params) => dispatch(getListPostAction(params)),
+});
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getListPost: (params) => dispatch(getListPostAction(params)),
-  }
-}
 export default connect(mapStateToProps, mapDispatchToProps)(App);
